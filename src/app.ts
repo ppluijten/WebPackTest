940fe77392a2b56e23c7d70f7e8eda24dab6abe6
Pieter-Paul Luijten <ppluijten@gmail.com>
2017-11-01 13:51:12 +0100

    Applying tslint suggestions.

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import { Foo } from "./foo";
 import * as foo from "./foo";
 import * as moment from "moment";
-import './styles.css';
-import './styles.scss';
-import './styles.less';
+import "./styles.css";
+import "./styles.scss";
+import "./styles.less";
 
 console.log(`Count: ${foo.Count}`);
 Foo.thisMethod("5");
 foo.LogMult(3, 6);
-var mult = foo.Multiply(3, 6);
+var mult: number = foo.Multiply(3, 6);
 console.log(mult);
 
-var formattedDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+var formattedDate: string = moment().format("MMMM Do YYYY, h:mm:ss a");
 console.log(formattedDate);
--- src/foo.ts
+++ src/foo.ts
@@ -1,16 +1,16 @@
-// Named exports
+// named exports
 export var Count: number = 5;
 
-export function Multiply(a: number, b: number) {
+export function Multiply(a: number, b: number): number {
   return a * b;
 }
 
-export function LogMult(a: number, b: number) {
+export function LogMult(a: number, b: number): void {
   console.log(Multiply(a, b));
 }
 
 export class Foo {
-  static thisMethod(param: any) {
+  static thisMethod(param: any): void {
     console.log(param);
   }
 }
